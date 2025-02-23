862b4e9cf8bbfec97d8c93ac7067569d739734a3
Vanitha-Tellakula <vanithatellakula@gmail.com>
2025-02-23 17:24:29 +0530

    Initial commit

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,68 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+     <img src="/ban.jpg" class="img-fluid w-100 pt-1" alt="..."/ >
+     <div class="row row-cols-1 row-cols-md-2 g-4 m-2">
+  <div class="col">
+    <div class="card">
+      <img src="https://www.erikastravelventures.com/wp-content/uploads/2021/11/cover-photos-21.jpg" class="card-img-top" alt="..."/>
+      <div class="card-body">
+        <h5 class="card-title">Veg Recipes</h5>
+        <p class="card-text">Explore a variety of delicious and nutritious vegetarian recipes that are easy to prepare and perfect for any occasion. Discover flavorful dishes that bring the best of plant-based ingredients to your table!.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card">
+      <img src="/non.jpg" class="card-img-top" alt="..."/ >
+      <div class="card-body">
+        <h5 class="card-title">Non-veg Recipes</h5>
+        <p class="card-text">Indulge in a wide selection of mouthwatering non-vegetarian recipes, from savory meats to flavorful seafood. Savor rich, hearty dishes that are sure to delight every palate!.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card">
+      <img src="https://www.archanaskitchen.com/images/archanaskitchen/basic_recipes/Amazing_Non_Veg_Appetizers__Starters_For_Your_New_Year_Party.jpg" class="card-img-top" alt="..."/ >
+      <div class="card-body">
+        <h5 class="card-title">Starter Recipes</h5>
+        <p class="card-text">Kick off your meals with a variety of tasty and tempting starter recipes. From crispy bites to savory dips, these appetizers are the perfect way to begin any feast!.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card">
+      <img src="https://www.truebasics.com/blog/wp-content/uploads/2023/07/heallthy-juices.jpg" class="card-img-top" alt="..." />
+      <div class="card-body">
+        <h5 class="card-title">Juice Recipes</h5>
+        <p class="card-text">"Refresh your day with a variety of vibrant and nutritious juice recipes, packed with natural flavors. From tropical blends to detoxifying green juices, each sip is a burst of health and freshness. Explore simple, yet delicious juices that are perfect for boosting your energy and hydration!.</p>
+      </div>
+    </div>
+  </div>
+
+<div class="col">
+    <div class="card">
+      <img src="https://www.foodanddrinkrecipes.com/wp-content/uploads/Soups-and-Stews-669x375.jpg" class="card-img-top" alt="..."/>
+      <div class="card-body">
+        <h5 class="card-title">Soups and stews Recipes</h5>
+        <p class="card-text">Warm up with a selection of comforting soups and hearty stews, perfect for cozy meals. From creamy classics to bold, flavorful concoctions, find your next favorite dish!.</p>
+      </div>
+    </div>
+</div>
+<div class="col">
+    <div class="card">
+      <img src="https://c.ndtvimg.com/2020-04/chd4rs3g_dessert_625x300_07_April_20.jpg" class="card-img-top" alt="..."/>
+      <div class="card-body">
+        <h5 class="card-title">Desserts</h5>
+        <p class="card-text">Satisfy your sweet tooth with a variety of indulgent dessert recipes, from rich cakes to refreshing fruit treats. Explore irresistible treats that are perfect for any occasion!.</p>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+  )
+}
+
+export default Home
